ae8a86275783f6116549295ba2437af425c250f5
aleks9045 <aleksey9045@gmail.com>
2025-06-17 18:36:29 +0300

    initial commit

---
+++ src/test/java/org/example/excelgeneration/ExcelGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.excelgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
